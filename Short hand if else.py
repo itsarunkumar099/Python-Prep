8db80bdfcca2f3d0670ee07f9a01f405267eb34f
Arun Kumar <126911771+itsarunkumar099@users.noreply.github.com>
2025-09-22 09:56:40 +0530

    Update Short hand if else.py

--- Short hand if else.py
+++ Short hand if else.py
@@ -1,4 +1,5 @@
 # ~Short hand if else~
+
 a = 330
 b = 3303
 print("A") if a > b else print("=") if a == b else print("B") # Short hand if else
@@ -8,4 +9,5 @@ print(c)
 
 c = 9 if a < b else 0 # Short hand if else to assign value to c
 print(c)
+
 
