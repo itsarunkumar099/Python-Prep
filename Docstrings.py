c6122278115f9a30b1f0066fdd618cdb55b04cae
Arun Kumar <126911771+itsarunkumar099@users.noreply.github.com>
2025-09-30 20:53:43 +0530

    Update Docstrings.py

--- Docstrings.py
+++ Docstrings.py
@@ -1,7 +1,9 @@
 # ~Docstrings~
+
 def square(n):
     '''Takes in a number n and returns the square of that number'''
     print(n**2)
 square(4)
 print(square.__doc__)  # Accessing the docstring of the function.
+
 
