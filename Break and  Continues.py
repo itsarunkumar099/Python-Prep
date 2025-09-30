5fd8891dd4c6fb7ecd6b51584195a10c76d9bf5e
Arun Kumar <126911771+itsarunkumar099@users.noreply.github.com>
2025-09-30 20:52:46 +0530

    Update Break and  Continues.py

--- Break and  Continues.py
+++ Break and  Continues.py
@@ -8,7 +8,7 @@ print("Loop ended")
 
 for i in range(15): 
     if (i == 10):
-        continue  # Skips the iteration when i is 10
+        continue  # Skips the iteration when i is 10.
     print("5 X ", i + 1, "=", 5 * (i + 1))
 
 i = 0 # Infinite loop example
@@ -16,4 +16,5 @@ while True:
     print(i)
     i = i + 1
     if(i%100 == 0):
+
         break
