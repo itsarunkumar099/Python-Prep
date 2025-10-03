d6873f4a67647245c7baf1525b3384b435cad08f
Arun Kumar <126911771+itsarunkumar099@users.noreply.github.com>
2025-10-03 20:22:52 +0530

    Update Enumerate Function.py

--- Enumerate Function.py
+++ Enumerate Function.py
@@ -2,17 +2,18 @@
 
 marks = [12, 15, 20, 1, 80, 70, 30, 25]
 
-index = 0             # Initializing index.
-for mark in marks:    # Iterating through the list.
+index = 0             # Initializing index
+for mark in marks:    # Iterating through the list
     print(mark)
     if(index==4):
         print("awesome")
-    index += 1        # Incrementing index.
+    index += 1        # Incrementing index
 
 # OR
 
-for index, mark in enumerate(marks):  # Using enumerate to get index and value.  
+for index, mark in enumerate(marks):  # Using enumerate to get index and value  
     print(mark)
     if(index==4):                     # Checking if index is 4
         print("awesome")
+
 
