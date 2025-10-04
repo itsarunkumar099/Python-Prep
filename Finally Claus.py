22fa3a94e032f8140c106935bd00474a7fdc3255
Arun Kumar <126911771+itsarunkumar099@users.noreply.github.com>
2025-10-04 20:34:03 +0530

    Update Finally Claus.py

--- Finally Claus.py
+++ Finally Claus.py
@@ -9,7 +9,7 @@ def func1():
         print("An error occurred.")
         return 0
     
-    finally: # Finally block will always execute    # regardless of whether an exception occurred or not.
+    finally: # Finally block will always execute    # regardless of whether an exception occurred or not
         print("Execution of func1 is complete.")
 
 x = func1()     
